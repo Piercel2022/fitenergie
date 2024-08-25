412db2697c950e328b23cccce2eecb354aaeb82a
Pierre Celestin Moussa <piercel.coder2022@gmail.com>
2024-08-26 00:20:30 +0200

    Add the workflow

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import logo from '../assets/images/logo.png';
 
 const Header = () => {
   return (
     <header className="bg-white shadow">
       <div className="container mx-auto px-4 py-4 flex justify-between items-center">
-        <div className="text-xl font-bold text-gray-800">VitaForme</div>
+        <div className="text-xl font-bold text-gray-800">
+        <img src={logo} width={150}   alt="logo"/></div>
         <nav>
           <ul className="flex space-x-6">
             <li><a href="#home" className="text-gray-600 hover:text-gray-800">Home</a></li>
